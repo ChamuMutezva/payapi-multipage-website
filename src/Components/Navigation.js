ec49b89c76131d00ff277d7391a9d269736ddbb2
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2021-04-25 23:59:15 +0200

    feat: styling tablet form

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -5,7 +5,7 @@ import Close from '../assets/shared/mobile/close.svg'
 import Buttton from './Button'
 const Navigation = () => {
 
-    const [ShowMenu, setShowMenu] = useState(true)
+    const [ShowMenu, setShowMenu] = useState(false)
     const hamburgerClick = () => {
         setShowMenu(!ShowMenu)
         console.log(ShowMenu)
@@ -41,10 +41,10 @@ const Navigation = () => {
                 <Buttton />
             </div>
             <div className="menu__control">
-                <button className={`hamburger ${ShowMenu ? "toggle__btn__close" : "toggle__btn__open"}`} onClick={hamburgerClick}>
+                <button className={`hamburger ${ShowMenu ? "toggle__btn__open" : "toggle__btn__close"}`} onClick={hamburgerClick}>
                     <img src={Hamburger} alt="open the menu" />
                 </button>
-                <button className={`close__menu ${ShowMenu ? "toggle__btn__open" : "toggle__btn__close"}`} onClick={hamburgerClick}>
+                <button className={`close__menu ${ShowMenu ? "toggle__btn__close" : "toggle__btn__open"}`} onClick={hamburgerClick}>
                     <img src={Close} alt="close the menu" />
                 </button>
             </div>
