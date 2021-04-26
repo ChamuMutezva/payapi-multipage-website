f8e1354678650ff891f1c156fc88ffbeb997d0be
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2021-04-26 20:09:22 +0200

    feat: created call to action component

---
+++ src/Components/CallToAction.js
@@ -0,0 +1,23 @@
+import Button from './Button'
+const CallToAction = () => {
+    return (
+        <div className="cta__summary">
+            <h1 className="cta__title">Start building with our APIs for absolutely free.</h1>
+            <form className="form">
+                <label className="sr-only" htmlFor="email">Email (Required):</label>
+                <input type="email"
+                    className="email" name="email"
+                    id="email" required aria-required="true"
+                    placeholder="Enter email address" />
+                <Button primary={true} />
+                <p className="form__questions">
+                    Have any questions?
+                              <a href="/" className="anchor form__anchor">
+                        Contact Us
+                                  </a>
+                </p>
+            </form>
+        </div>
+    )
+}
+export default CallToAction
