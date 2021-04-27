9309408ca437fdc289390e8d87ca0aa78123176e
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2021-04-27 19:52:11 +0200

    feat: seccondary financial cards layout

---
+++ src/Components/Card.js
@@ -0,0 +1,14 @@
+const Card = (props) => {
+   const primaryImg = props.primary ? "js__primaryImg" : ""
+    return (
+        <div className="card">
+            <div className={`card__holder  ${primaryImg}`}><img className="card__img" src={props.src} alt={props.alt} /></div>
+            <div className="card__description">
+                <h2 className="card__title">{props.title}</h2>
+                <h3 className="card__subtitle">{props.subtitle}</h3>
+                <p className="card__summary">{props.summary}</p>
+            </div>
+        </div>
+    )
+}
+export default Card
--- src/pages/home/main/Secondary.js
+++ src/pages/home/main/Secondary.js
@@ -1,17 +1,42 @@
-import CardImg from "../../../assets/home/desktop/illustration-easy-to-implement.svg"
+import Card from "../../../Components/Card"
+import CardImplementation from "../../../assets/home/desktop/illustration-easy-to-implement.svg"
+import PhoneUx from "../../../assets/home/desktop/illustration-simple-ui.svg"
+import PersonalFinances from "../../../assets/home/desktop/icon-personal-finances.svg"
+import Bankings from "../../../assets/home/desktop/icon-banking-and-coverage.svg"
+import Consumer from "../../../assets/home/desktop/icon-consumer-payments.svg"
+
 const Secondary = () => {
     return (
-        <section>
-            <div className="card">
-                <img src={CardImg} alt="" />
-                <h2 className="card__title">Easy to implement</h2>
-
-                <p className="card__summary">
-                    Our API comes with just a few lines of code. You’ll be up and running in
-                    no time. We built our documentation page to integrate payments functionality
-                    with ease.
-                 </p>
+        <section className="secondary__cards">
+            <div className="cards__one">
+                <Card src={CardImplementation} alt="illustration on implementation"
+                    title="Easy to implement"
+                    summary="Our API comes with just a few lines of code. You’ll be up and running in
+                             no time. We built our documentation page to integrate payments functionality
+                             with ease."
+                />
+                <Card src={PhoneUx} alt=""
+                    title="Simple UI & UX"
+                    summary="Our pre-built form is easy to integrate in your app or website’s checkout 
+                            flow and designed to optimize conversion."
+                />
             </div>
+            <div className="cards__two">
+                <Card src={PersonalFinances} alt="personal finances steps" 
+                        title="" subtitle="Personal Finances" primary={true}
+                         summary=" Consolidate financial data from multiple sources and categorize transactions up to
+                         2 years of history. Analyze reports to reconcile activities in your account."/>
+                <Card src={Bankings} alt="types of banking and coverage" 
+                        title="" subtitle="Banking & Coverage" primary={true}
+                         summary="With our platform, you can speed up account onboarding and support ongoing payments 
+                        for checking, savings, credit card, and brokerage accounts."/>
+                <Card src={Consumer} alt="banking payments systems" 
+                        title="" subtitle="Consumer Payments" primary={true}
+                         summary="It’s easier to set up secure bank payments with us through a flow designed with the 
+                         user experience in mind. Customers could instantly authenticate their account."/>
+
+            </div>
+
         </section>
     )
 }
