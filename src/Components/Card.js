32c4ce7bee56492deac4c457e66efa5d608489a5
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2021-04-28 21:26:39 +0200

    feat: cards section - tablet mode implemented

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -2,7 +2,7 @@ const Card = (props) => {
    const primaryImg = props.primary ? "js__primaryImg" : ""
     return (
         <div className="card">
-            <div className={`card__holder  ${primaryImg}`}><img className="card__img" src={props.src} alt={props.alt} /></div>
+        <div className={`card__holder`}><img className={`card__img ${primaryImg}`} src={props.src} alt={props.alt} /></div>
             <div className="card__description">
                 <h2 className="card__title">{props.title}</h2>
                 <h3 className="card__subtitle">{props.subtitle}</h3>
