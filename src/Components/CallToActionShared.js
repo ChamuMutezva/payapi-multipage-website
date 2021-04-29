8f46ec45247001c8f91bf61508880c0bae15c830
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2021-04-29 04:12:52 +0200

    feat: call to action component created

---
+++ src/Components/CallToActionShared.js
@@ -0,0 +1,17 @@
+import Button from './Button'
+const CallToActionShared = (props) => {
+    return (
+        <div className="cta__summary cta__summary__shared">
+            <h2 className="cta__title">Ready to start</h2>
+            <form className="form">
+                <label className="sr-only" htmlFor="email">Email (Required):</label>
+                <input type="email"
+                    className="email" name="email"
+                    id="email-address" required aria-required="true"
+                    placeholder="Enter email address" />
+                <Button primary={true} />                
+            </form>
+        </div>
+    )
+}
+export default CallToActionShared
--- src/pages/home/main/Main.js
+++ src/pages/home/main/Main.js
@@ -1,10 +1,12 @@
 import Hero from './Hero'
 import Secondary from './Secondary'
+import CallToActionShared from '../../../Components/CallToActionShared'
 const Main = () => {
     return (
         <main className="main">
             <Hero />
             <Secondary />
+            <CallToActionShared />
         </main>
     )
 }
