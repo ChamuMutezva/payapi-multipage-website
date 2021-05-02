d8d84b1c6f67c7c8b3238e9e0f7c5f57ea44f9e0
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2021-05-02 12:29:20 +0200

    feat: pricing page desktop page initial layout

--- src/pages/pricing/pricing_page.js
+++ src/pages/pricing/pricing_page.js
@@ -1,10 +1,12 @@
 import PricingHeader from '../pricing/header/pricing_header'
 import MainPricing from './main/MainPricing'
+import Footer from '../../Components/Footer'
 const PricingPage = () => {
     return(
         <div>
             <PricingHeader />
             <MainPricing />
+            <Footer />
         </div>
     )
 }
