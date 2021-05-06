f7008be330a7a73c615be244e456cafe30e7cc80
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2021-05-06 05:19:26 +0200

    form setup -onchange and onsubmit events

--- src/pages/contact/MainContact/FormContact.js
+++ src/pages/contact/MainContact/FormContact.js
@@ -1,20 +1,59 @@
+import { useState } from 'react'
 import Button from '../../../Components/Button'
+
 const FormContact = () => {
+
+  const initialState = {
+    fullName: "",
+    email: "",
+    company: "",
+    title: "",
+    message: "",
+    terms: ""
+
+  }
+
+  const [formData, setData] = useState(initialState)
+
+  const onChange = (e) => {
+    const { name, value, type, checked } = e.target
+    if (type === "checked") {
+      setData({ ...formData, [name]: checked })
+    } else {
+      setData({ ...formData, [name]: value })
+    }
+  }
+
+  const onBlur = (e) => {
+    const { name } = e.target
+    setData({ ...formData, touched: { ...formData.touched, [name]: true } })
+  }
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault()
+    console.log(formData)
+  }
+
+  const { fullName, email, company, title, message, terms } = formData
   return (
-    <form className="form form__contact">
+    <form className="form form__contact" onSubmit={handleSubmit}>
 
       {/*name */}
       <div className="details">
         <input
           className="form__inputs"
-          type="name"
-          name="firstname"
+          type="text"
+          name="fullName"
           aria-required="true"
-          id="firstname"
+          required
+          id="fullname"
+          value={fullName}
+          onChange={onChange}
+          onBlur={onBlur}
         />
-        <label className="input__label name__label" htmlFor="firstname">
+        <label className="input__label name__label" htmlFor="fullname">
           Name
-                 </label>
+         </label>
 
       </div>
 
@@ -25,12 +64,15 @@ const FormContact = () => {
           type="email"
           name="email"
           aria-required="true"
+          required
           id="email"
+          value={email}
+          onChange={onChange}
 
         />
         <label className="input__label" htmlFor="email">
           Email
-                </label>
+         </label>
 
       </div>
 
@@ -39,14 +81,17 @@ const FormContact = () => {
         <input
           className="form__inputs"
           type="text"
-          name="companyname"
+          name="company"
           aria-required="true"
+          required
           id="companyname"
+          value={company}
+          onChange={onChange}
 
         />
         <label className="input__label name__label" htmlFor="companyname">
           Company Name
-                 </label>
+         </label>
 
       </div>
 
@@ -55,9 +100,12 @@ const FormContact = () => {
         <input
           className="form__inputs"
           type="text"
-          name="titled"
+          name="title"
           aria-required="true"
+          required
           id="titled"
+          value={title}
+          onChange={onChange}
 
         />
         <label className="input__label name__label" htmlFor="titled">
@@ -68,23 +116,31 @@ const FormContact = () => {
 
       {/*message*/}
       <div className="details">
-        <textarea name="msg"
-         className="form__inputs"
-         id="message"
-        cols="30" rows="3"></textarea>
-       
+        <textarea name="message"
+          className="form__inputs"
+          id="message"
+          value={message}
+          onChange={onChange}
+          cols="30"
+          rows="3"></textarea>
+
         <label className="input__label name__label" htmlFor="msg">
           Message
         </label>
 
       </div>
       <div className="details details__check">
-        <input  className="form__check" type='checkbox' id='update' name='update' />
-        <label htmlFor='update' className="check__label">
+        <input className="form__check"
+          value={terms}
+          onChange={onChange}
+          type='checkbox'
+          id='terms'
+          name='terms' />
+        <label htmlFor='terms' className="check__label">
           Stay up-to-date with company announcements and updates to our API
           </label>
       </div>
-      <Button title="Submit" formBtn={true}/>
+      <Button title="Submit" formBtn={true} />
     </form>
   )
 }
