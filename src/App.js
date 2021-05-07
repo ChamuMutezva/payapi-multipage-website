400b046f1d7b83338f9584a49b087d2bc1f9342c
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2021-05-07 20:49:09 +0200

    feat: scroll to component implemented

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import { useEffect } from 'react'
+import { BrowserRouter as Router, Switch, Route , useLocation} from "react-router-dom"
 import './Sass/App.scss'
 import HomePage from './pages/home/home_page'
 import AboutPage from './pages/about/about_page'
 import ContactPage from './pages/contact/contact_page'
 import PricingPage from './pages/pricing/pricing_page'
+
+//Function to scroll to top of page when navigating 
+//to a new page. at the end of a page focus of page will
+//be at the bottom of the page
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+console.log(pathname)
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 function App() {
+ 
   return (
     <div className="App">
       <Router>
+      <ScrollToTop />
         <Switch>
           <Route exact path="/">
             <HomePage />
--- src/pages/about/MainAbout/HeroAbout.js
+++ src/pages/about/MainAbout/HeroAbout.js
@@ -4,9 +4,9 @@ import deskTeam from '../../../assets/about/desktop/image-team-members.jpg'
 const HeroAbout = () => {
     return(
         <picture className="about__img">            
-            <source media="(max-width:719px)" srcset={mobileTeam}/>
-            <source media="(max-width:1279px)" srcset={tabletTeam}/>
-            <source media="(min-width: 1280px)" srcset={deskTeam} />
+            <source media="(max-width:719px)" srcSet={mobileTeam}/>
+            <source media="(max-width:1279px)" srcSet={tabletTeam}/>
+            <source media="(min-width: 1280px)" srcSet={deskTeam} />
             <img className="hero__about__img" src={mobileTeam} alt="team members at work"/>
         </picture>
     )
